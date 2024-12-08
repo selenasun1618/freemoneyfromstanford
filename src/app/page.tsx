4a0abfcbb614e36822fe105cca1ecb5f585be52c
selenasun1618 <selenasun02@gmail.com>
2024-12-08 10:40:32 +0000

    fixed minor spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,8 +52,8 @@ export default function Home() {
             <div className="bg-digital-red w-full pt-10">
                 <div className="container mx-auto md:p-4 md:pb-10">
                     <h1 className={"text-center text-4xl font-bold my-6 px-1 text-white"}>
-                        <span className={'inline-block'}>Need money for a <span
-                            className={"underline"}>project</span>, </span>
+                    <span className={'inline-block'}>Need money for a </span>{' '}
+                        <span className={"underline"}>project</span>,{' '}
                         <span className={'inline-block'}>but unsure how to fund it?</span>
                     </h1>
                     <p className={"text-center text-lg font-medium mb-6 mt-9 px-1 block"}>
@@ -68,17 +68,13 @@ export default function Home() {
 
                     <div className={"flex flex-col grow sm:rounded-2xl bg-digital-red px-5 pb-5"}>
                         <div className={"my-2 mx-3 flex flex-row grow"}>
-                            <input type="search" placeholder="Search your grant topic..." enterKeyHint="search"
+                            <input type="search" placeholder="Enter your project idea..." enterKeyHint="search"
                                    name="search"
                                    value={searchState.searchString}
                                    onChange={(e) => setSearchState({ searchString: e.target.value })}
                                    className={"rounded-l-3xl py-3 pl-6 pr-3 flex-grow bg-slate-100 text-black-1000"}
                             />
                         </div>
-                        <span className={"text-center mt-1 my-2 text-black-100"}>
-                        Or go directly to our&nbsp;{/*<a href="/search" className={`underline text-white`}>Advanced Search</a>&nbsp; */}
-                            {/*page, or our&nbsp;*/}<a href="/list" className={`underline text-white cursor-pointer`}>list of grants</a>
-                        </span>
                     </div>
                 </div>
             </div>
